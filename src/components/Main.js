96673c39970d981746879709f5c93868bbf6b7ca
Arrangedgodly <90996993+Arrangedgodly@users.noreply.github.com>
2022-09-30 00:12:47 -0600

    got a huge stride on the art gallery coming back online and actually being smooth as butter

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,19 @@
-    import Header from "./Header";
+import React from "react";
+import Header from "./Header";
 import Body from "./Body";
+import Modal from "./Modal";
 
 function Main() {
+    const [selectedCard, setSelectedCard] = React.useState();
+    const [isModalOpen, setIsModalOpen] = React.useState(false);
+
     return (
         <main>
             <Header />
             <Body />
+            {isModalOpen && (
+                <Modal />
+            )}
         </main>
     );
 }
